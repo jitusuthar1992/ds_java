425088a4e6afb36f6ff9cd4287b28453f87af7c4
Bhaskar Suthar <bhaskar.suthar@snapdeal.com>
2020-03-01 10:25:50 +0530

    element appear only once

---
+++ src/main/array/ElementAppearsOnce.java
@@ -0,0 +1,18 @@
+package main.array;
+
+public class ElementAppearsOnce {
+
+    static int findElement(int arr[]){
+
+        int res =0;
+        for (int i = 0; i < arr.length ; i++) {
+            res = res ^ arr[i];
+        }
+        return res;
+    }
+    public static void main (String[] args)
+    {
+        int ar[] = {2, 3, 5, 4, 5, 3, 4};
+        System.out.println(findElement(ar));
+    }
+}
