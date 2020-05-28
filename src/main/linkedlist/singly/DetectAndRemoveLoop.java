9fe66b1211f973e22adb748201c6a0c3b7d28c05
jitendra.kumar01 <jitendra.lumar01@snapdeal.com>
2020-05-28 17:07:04 +0530

    swap nodes in linked list

--- src/main/linkedlist/singly/DetectAndRemoveLoop.java
+++ src/main/linkedlist/singly/DetectAndRemoveLoop.java
@@ -58,19 +58,51 @@ public class DetectAndRemoveLoop {
         public static void main(String[] args) {
             SinglyLinkedList list = new SinglyLinkedList();
 
-            list.head = new ListNode(50);
-            list.head.setNext(new ListNode(20));
-            list.head.getNext().setNext(new ListNode(15));
-            list.head.getNext().getNext().setNext(new ListNode(4));
-            list.head.getNext().getNext().getNext().setNext(new ListNode(10));
-            list.head.getNext().getNext().getNext().getNext().setNext(list.head.getNext().getNext());
+            list.head = new ListNode(12);
+            list.head.setNext(new ListNode(99));
+            list.head.getNext().setNext(new ListNode(13));
+            list.head.getNext().getNext().setNext(new ListNode(5));
+            list.head.getNext().getNext().getNext().setNext(new ListNode(25));
+            list.head.getNext().getNext().getNext().getNext().setNext(list.head);
 
            // System.out.println("Linked List before removing loop : ");
            // list.printList(list.head);
 
-            DetectAndRemoveLoop.detectLoopAndRemovesol1(list.head);
+            DetectAndRemoveLoop.removeLoopSol(list.head);
             System.out.println("Linked List after removing loop : ");
            list.printList(list.head);
+
+        }
+
+
+        public static void removeLoopSol(ListNode head){
+            ListNode slow ,fast;
+            slow=fast=head;
+            while(fast.next != null && fast.next.next!= null){
+                slow = slow.next;
+                fast = fast.next.next;
+                if(slow.data == fast.data){
+                    break;
+                }
+            }
+
+            if(fast == slow){
+                if(fast == head){
+                    while (slow.next != fast){
+                        slow= slow.next;
+                    }
+
+                    slow.next = null;
+                }else {
+                    slow = head;
+                    while (slow.next != fast.next){
+                        slow = slow.next;
+                    }
+                    fast.next = null;
+                }
+            }
+
+
 
         }
 
---
+++ src/main/linkedlist/singly/SwapTwoNode.java
@@ -0,0 +1,71 @@
+package main.linkedlist.singly;
+
+public class SwapTwoNode {
+    public static ListNode swap(ListNode head , int a , int b ){
+        if(null== head || null== head.next)
+            return null;
+
+        ListNode prev1 , prev2, t1,t2, node;
+
+        node = head;
+
+        prev1 = prev2=t1=t2=null;
+
+        while (null != node){
+
+            if(node.data==a){
+                t1= node;
+                break;
+            }
+
+            prev1 = node;
+            node = node.next;
+        }
+
+
+        while (null != node){
+
+            if(node.data==b){
+                t2= node;
+                break;
+            }
+
+            prev2 = node;
+            node = node.next;
+        }
+
+
+        if(null == t1 || null == t2)
+            return head;
+
+        if(prev1 != null){
+            prev1.next = t2;
+        }else{
+            head = t2;
+        }
+
+        if(prev2 != null){
+            prev2.next = t1;
+        }else{
+            head = t1;
+        }
+
+        ListNode temp = t1.next;
+        t1.next = t2.next;
+        t2.next = temp;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.head = new ListNode(12);
+        list.head.setNext(new ListNode(99));
+        list.head.getNext().setNext(new ListNode(8));
+        list.head.getNext().getNext().setNext(new ListNode(39));
+        list.head.getNext().getNext().getNext().setNext(new ListNode(5));
+        list.printList(swap(list.head,99,39));
+    }
+}
