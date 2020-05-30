efe63738499f08142eb58e076fecb60625b9fc4a
jitendra.kumar01 <jitendra.lumar01@snapdeal.com>
2020-05-30 18:17:52 +0530

    check connected undirected graph

---
+++ src/main/graph/CheckConnectedUndirectedGraph.java
@@ -0,0 +1,68 @@
+package main.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class CheckConnectedUndirectedGraph {
+    List<List<Integer>> graph;
+    int nodes;
+    boolean visited[];
+
+    public CheckConnectedUndirectedGraph(int nodes) {
+        this.nodes = nodes;
+        visited = new boolean[nodes];
+
+        for (int i = 0; i <nodes ; i++) {
+            graph.add(i, new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int a , int b){
+        graph.get(a).add(b);
+        graph.get(b).add(a);
+    }
+
+    public boolean isGraphConnected(){
+        dfs(0);
+        for (int i = 0; i <nodes ; i++) {
+            if(!visited[i])
+                return false;
+        }
+        return true;
+    }
+    public void dfs(int start){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(start);
+        visited[start]= true;
+
+        while (!stack.isEmpty()){
+            Integer node = stack.pop();
+            List<Integer> neighbours = graph.get(node);
+
+            for (Integer neighbour: neighbours) {
+                if(!visited[neighbour]){
+                    stack.push(neighbour);
+                    visited[neighbour]= true;
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes = 7;
+
+        CheckConnectedUndirectedGraph a = new CheckConnectedUndirectedGraph(nodes);
+
+        a.addEdge(0, 1);
+        a.addEdge(0, 2);
+        a.addEdge(1, 3);
+        a.addEdge(2, 4);
+        a.addEdge(3, 5);
+        a.addEdge(4, 5);
+        //a.addEdge(4, 6);
+
+        System.out.println(a.isGraphConnected());
+    }
+}
