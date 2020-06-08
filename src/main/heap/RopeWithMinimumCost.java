b4fac22a5907f56a07a824d8cdc7643738c2a6ec
jitendra.kumar01 <jitendra.lumar01@snapdeal.com>
2020-06-08 15:27:43 +0530

    RopeWithMinimumCost

---
+++ src/main/heap/RopeWithMinimumCost.java
@@ -0,0 +1,29 @@
+package main.heap;
+
+import java.util.PriorityQueue;
+
+public class RopeWithMinimumCost {
+
+    public static int connectRopeWithMinimumCost(int[] ropes) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i <ropes.length ; i++) {
+            minHeap.add(ropes[i]);
+        }
+
+        int minCost = 0;
+
+        while (minHeap.size()>1){
+            int tempCost = minHeap.poll()+minHeap.poll();
+            minCost+= tempCost;
+            minHeap.add(tempCost);
+        }
+
+        return minCost;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {3, 4, 7, 2};
+        System.out.println(RopeWithMinimumCost.connectRopeWithMinimumCost(arr));
+    }
+}
