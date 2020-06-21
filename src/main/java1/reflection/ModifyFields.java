ab52f98c095b109add12528ec0535f34833a8bce
jitendra.kumar01 <jitendra.lumar01@snapdeal.com>
2020-06-21 09:45:05 +0530

    modify static final field

---
+++ src/main/java1/reflection/ModifyFields.java
@@ -0,0 +1,36 @@
+package main.java1.reflection;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+public class ModifyFields    {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        Field field = Test.class.getDeclaredField("check");
+        field.setAccessible(true);
+        //field.set(null,true);
+
+        field.setAccessible(true);
+        int modifiers = field.getModifiers();
+        Field modifierField = field.getClass().getDeclaredField("modifiers");
+        modifiers = modifiers & ~Modifier.FINAL;
+        modifierField.setAccessible(true);
+        modifierField.setInt(field, modifiers);
+
+        field.set(field,true);
+
+        System.out.println(Test.getCheck());
+    }
+
+}
+
+
+class Test {
+    private static final Boolean check = false;
+
+    public Test() {
+    }
+
+    public static Boolean getCheck() {
+        return check;
+    }
+}
