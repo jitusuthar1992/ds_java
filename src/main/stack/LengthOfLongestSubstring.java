4db606732de8ecad18ed471e81e35418a1b24eff
jitusuthar1992 <jitendravishwakamra19@gmail.com>
2020-07-03 14:16:09 +0530

    length of longest substring

---
+++ src/main/stack/LengthOfLongestSubstring.java
@@ -0,0 +1,50 @@
+package main.stack;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LengthOfLongestSubstring {
+    public static int lengthOfLongestSubstringSol1(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j <= n; j++)
+                if (allUnique(s, i, j)) ans = Math.max(ans, j - i);
+        return ans;
+    }
+
+    public static boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
+        }
+        return true;
+    }
+    public static void main(String[] args)
+    {
+        String str = "geeksforgeeks";
+        System.out.println("The input string is " + str);
+        int len =lengthOfLongestSubstringSol2(str);
+        System.out.println("The length of "
+                + "the longest non repeating character is " + len);
+    }
+
+    public static int lengthOfLongestSubstringSol2(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
