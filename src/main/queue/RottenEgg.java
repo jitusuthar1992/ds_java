5bb5fd4c7b7065286c5c5f3f801a4d43f47795bf
jitendra.kumar1 <jitendra.kumar1@ihsmarkit.com>
2023-05-22 19:55:41 +0530

    stack and queue problems

--- src/main/queue/RottenEgg.java
+++ src/main/queue/RottenEgg.java
@@ -7,7 +7,7 @@ public class RottenEgg {
 
     public static void main(String[] args) {
         int arr[][] ={
-                        {0,1,2},
+                        {1,0,2},
                         {0,1,2},
                         {1,1,1}
                      }   ;
